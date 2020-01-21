be3e7368a1d8b92b8d35edeea83e699040c84722
Poosana Thassanavisut <poosana@Poosanas-MacBook-Pro.local>
2020-01-21 16:23:39 +0700

    Done

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -4,22 +4,36 @@
 using namespace std;
 
 //[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade(float x){
+	char output;
+	if(x>90) output = 'A';
+	else if(x>75) output = 'B';
+	else if(x>60) output = 'C';
+	else if (x>45) output = 'D';
+	else output = 'F';
+	return output;
+
+}
 
 int main(){
 	//Input the number of students
 	int N,i = 0;
 	cout << "Enter the number of students: ";
 	cin >> N;
-	string name[N];
-	float score[N];	
+	string name[N],Inname;
+	float score[N],Inscore;	
 	
 	//Store names and scores of students into an array 
 	while(i < N){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
-		//[Missing Code 2] Get name of the i-th students that may include whitespace.
-		cout << "Score of student " << i+1 << ": ";
-		//[Missing Code 3] Get score of the i-th students.
+		
+		getline(cin,Inname);
+		cout <<"score of student "<<i+1<<":";
+		cin >> Inscore;
+		name[i] = Inname;
+		score[i] = Inscore;
+		
 		i++;
 	}
 	
--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,1 +1,43 @@
 //status = "ONE-NIGHT-STAND";
+#include<iostream>
+#include<string>
+ 
+using namespace std;
+int main(){
+    string status;
+    int age,height,money;
+    cout << "How old are you: ";
+    cin >> age;
+    if(age <20){
+        cout << "How tall are you: ";
+        cin >> height;
+        if (height<160)
+        {
+            status = "UNFRIEND";
+        }else if(height <175)
+        {
+            status = "FRIEND";
+        }else{
+            cout <<"How much money do you have: ";
+            cin >> money;
+            if(money>69000000){
+                status = "MARRIED";
+            }else{
+                status = "ONE-NIGHT-STAND";
+            }
+        }
+
+}    else if(age<30){
+        cout <<"How much money do you have: ";
+        cin >>money;
+        if(money >10000000){
+            status ="BESTFRIEND";
+        }else
+        {status = "UNFRIEND";
+        }
+    }else{status = "UNFRIEND";
+        }
+        cout << status;
+
+}   
+
